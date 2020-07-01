85e13d6028abd16660dcb0fd37653129249a029d
CO-adri <adri.cr@hotmail.fr>
2020-07-01 21:21:31 +0200

    First commit

---
+++ public/scripts/main.js
@@ -0,0 +1,12 @@
+// etre sur que le navigateur supporte les services workers
+if ('serviceWorker' in navigator) {
+// inscription du service workler
+window.addEventListener('load', () => {
+navigator.serviceWorker
+.register('/sw.js')
+.then (reg => console.log("Service worker : Register"))
+.catch(err=> console.log("Service worker: Error ${err}"))
+})
+};
+
+
